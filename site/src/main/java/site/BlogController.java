93f3474cd6102a07aa0a4090176aa1067d9757d6
isho <idoshoshani@gmail.com>
2017-06-25 13:25:05 -0700

    with paths

--- site/src/main/java/site/BlogController.java
+++ site/src/main/java/site/BlogController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.Calendar;
 import java.util.Date;
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,29 +14,40 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(path="/blog")
 public class BlogController{
+
   private final AtomicLong counter = new AtomicLong();
   @Autowired
   private BlogRepository blogRepository;
 
 
-  @RequestMapping("/add")
-  public @ResponseBody String addNewBlog(@RequestParam String title){
-    Date currentDate = new Date();
+  @RequestMapping(value = "/add")
+  public @ResponseBody String addNewBlog(@RequestParam String title, @RequestParam String body){
     Calendar today = Calendar.getInstance();
     int month = today.get(today.MONTH);
     int year = today.get(today.YEAR);
-    Blogs n = new Blogs(counter.incrementAndGet(),title,month,year,currentDate,currentDate);
+    Blog n = new Blog();
+    n.title = title;
+    n.month= month;
+    n.year = year;
+    n.body = body;
     blogRepository.save(n);
     return "Saved";
   }
-  @RequestMapping("/find")
-  public @ResponseBody Blogs findBlog(@RequestParam long id){
+  @RequestMapping(value = "/edit")
+  public @ResponseBody String EditBlog(@RequestParam String title, @RequestParam String body, @RequestParam long id){
+    Blog updated = blogRepository.findOne(id);
+    updated.title = title;
+    updated.body = body;
+    blogRepository.save(updated);
+    return "Saved";
+  }
+  @RequestMapping(value = "/find")
+  public @ResponseBody Blog findBlog(@RequestParam long id){
     return blogRepository.findOne(id);
   }
 
-
-  @RequestMapping("/all")
-  public @ResponseBody Iterable<Blogs> getAll(){
+  @RequestMapping(value = "/all")
+  public @ResponseBody Iterable<Blog> getAll(){
     return blogRepository.findAll();
   }
 
